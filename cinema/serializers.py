f26b8dde7444ffcab80a1200abe7139b6bfee113
viloker <yarmolykvadym@gmail.com>
2026-05-18 14:42:27 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -29,6 +29,9 @@ class MovieSerializer(serializers.Serializer):
         return movie
 
     def update(self, instance, validated_data):
+        actors = validated_data.get("actors")
+        genres = validated_data.get("genres")
+
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get(
             "description", instance.description
@@ -36,6 +39,12 @@ class MovieSerializer(serializers.Serializer):
         instance.duration = validated_data.get("duration", instance.duration)
 
         instance.save()
+
+        if actors is not None:
+            instance.actors.set(actors)
+
+        if genres is not None:
+            instance.genres.set(genres)
 
         return instance
 
